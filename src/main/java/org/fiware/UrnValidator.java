c2b212b0483aca7c390095cddff8972d5724197d
Jose M. Cantera <jmcanterafonseca@gmail.com>
2018-03-08 23:18:53 +0100

    Update UrnValidator.java

--- src/main/java/org/fiware/UrnValidator.java
+++ src/main/java/org/fiware/UrnValidator.java
@@ -1,6 +1,16 @@
 package org.fiware;
 import java.util.regex.Pattern;
 
+/**
+ *
+ *  URN Validator
+ *
+ *   Copyright (c) 2017-18 FIWARE Foundation e.V.
+ *
+ *   LICENSE: MIT
+ *
+ *
+ */
 public class UrnValidator {
     public final static Pattern URN_PATTERN = Pattern.compile(
             "^urn:[a-z0-9][a-z0-9-]{0,31}:([a-z0-9()+,\\-.:=@;$_!*']|%[0-9a-f]{2})+$",
