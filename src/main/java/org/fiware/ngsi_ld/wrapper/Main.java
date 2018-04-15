c1f7c0420cada420eb9d1de7ec39ebe3370b637c
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-04-15 18:19:32 +0200

    Remove option to kill server when pressing a key

--- src/main/java/org/fiware/ngsi_ld/wrapper/Main.java
+++ src/main/java/org/fiware/ngsi_ld/wrapper/Main.java
@@ -56,8 +56,6 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
     }
 }
 
